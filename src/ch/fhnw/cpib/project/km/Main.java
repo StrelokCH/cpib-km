d40964be4746bb265febec7ffc0a53fa05c855cc
Janik Meyer <janik.my@gmail.com>
2018-09-26 12:34:03 +0200

    setup project including JUnit

---
+++ src/ch/fhnw/cpib/project/km/Main.java
@@ -0,0 +1,13 @@
+package ch.fhnw.cpib.project.km;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static int foo() {
+		return 42;
+	}
+
+}
---
+++ test/ch/fhnw/cpib/project/km/MainTest.java
@@ -0,0 +1,14 @@
+package ch.fhnw.cpib.project.km;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	@Test
+	void test() {
+		assertEquals(42, Main.foo());
+	}
+
+}
