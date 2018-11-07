e605e1365e998ed3a83461cd09575406b01b18e1
Janik Meyer <janik.meyer@students.fhnw.ch>
2018-11-07 16:50:13 +0100

    added more imports in ParserGenerator

--- src/ch/fhnw/cpib/project/km/parser/generator/Starter.java
+++ src/ch/fhnw/cpib/project/km/parser/generator/Starter.java
@@ -5,7 +5,9 @@ public class Starter {
 		String parseTablePath = "Grammar_IML_ParseTable.txt";
 		String outDir = "Generated-Parser/";
 		String javaFileHeader = "package ch.fhnw.cpib.project.km.syntax.concrete;\r\n\r\n" + 
-				"import ch.fhnw.cpib.project.km.token.*;";
+				"import ch.fhnw.cpib.project.km.token.keywords.*;\r\n" +
+				"import ch.fhnw.cpib.project.km.token.symbols.*;\r\n" +
+				"import ch.fhnw.cpib.project.km.token.various.*;\r\n";
 		
 		ParserGenerator g = new ParserGenerator(parseTablePath);
 		g.generate(outDir, javaFileHeader);
