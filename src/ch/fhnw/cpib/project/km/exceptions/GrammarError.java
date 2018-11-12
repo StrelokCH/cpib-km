b9b3cca9215a9116b24d57576f0cf854bfe7601f
Aude Kauffmann <aude.kauffmann@hs-furtwangen.de>
2018-11-12 15:32:58 +0100

    started with Parser, still errors

---
+++ src/ch/fhnw/cpib/project/km/exceptions/GrammarError.java
@@ -0,0 +1,14 @@
+package ch.fhnw.cpib.project.km.exceptions;
+
+public class GrammarError extends Exception {
+
+	public GrammarError(String string) {
+		super(string);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
