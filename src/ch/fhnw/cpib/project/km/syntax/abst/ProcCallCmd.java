1f92bc48cdfb3d7a2fe954f40ce3173daf3639e7
Aude Kauffmann <aude.kauffmann@hs-furtwangen.de>
2018-12-04 12:27:02 +0100

    fix ProCallCmd

--- src/ch/fhnw/cpib/project/km/syntax/abst/ProcCallCmd.java
+++ src/ch/fhnw/cpib/project/km/syntax/abst/ProcCallCmd.java
@@ -1,22 +1,30 @@
 package ch.fhnw.cpib.project.km.syntax.abst;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ch.fhnw.cpib.project.km.token.various.Identifier;
 
 public class ProcCallCmd implements ICommand {
 	
-	private final Identifier identifier;
-	/* -- TO-DO: -- 
-		routineCall
-		[identifier]*/
+	private final Identifier identifierProc;
+	private final List<IExpression> parameters;
+	private final List<Identifier> identifiers;
 	
-	public ProcCallCmd(Identifier identifier) {
+
+	
+	public ProcCallCmd(Identifier identifierProc, List<IExpression> parameters, List<Identifier> identifiers) {
 		super();
-		this.identifier = identifier;
+		this.identifierProc = identifierProc;
+		this.parameters = parameters;
+		this.identifiers = identifiers;
 	}
-	
+
+
 	@Override
 	public String toString(String indent) {
-		return indent + "(" + this.getClass().getSimpleName() + ", " + identifier.toString() + ")\n";
+		return indent + "(" + this.getClass().getSimpleName() + ", " + identifierProc.toString() + ")\n";
+		//To-Do show parameters and identifiers
 	}
 	
 }
--- src/ch/fhnw/cpib/project/km/syntax/abst/SkipCmd.java
+++ src/ch/fhnw/cpib/project/km/syntax/abst/SkipCmd.java
@@ -2,7 +2,6 @@ package ch.fhnw.cpib.project.km.syntax.abst;
 
 public class SkipCmd implements IExpression {
 
-	//Nothing ?
 	@Override
 	public String toString(String indent) {
 		return indent + "(" + this.getClass().getSimpleName() + ")\n";
