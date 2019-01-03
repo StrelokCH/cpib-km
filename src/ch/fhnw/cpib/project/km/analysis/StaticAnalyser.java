21d8e70939ddaa83f337c8c1e6ae044ec917ee51
Janik Meyer <janik.meyer@students.fhnw.ch>
2019-01-03 15:50:42 +0100

    extended StaticAnalyser to improve reusability

--- src/ch/fhnw/cpib/project/km/analysis/StaticAnalyser.java
+++ src/ch/fhnw/cpib/project/km/analysis/StaticAnalyser.java
@@ -4,19 +4,33 @@ import ch.fhnw.cpib.project.km.exceptions.ContextCheckingException;
 import ch.fhnw.cpib.project.km.syntax.abst.Program;
 
 public class StaticAnalyser {
+
 	private final Program program;
+	private Environment env;
+	private boolean isValid = false;
 
 	public StaticAnalyser(Program program) {
 		this.program = program;
 	}
 
 	public boolean check() throws ContextCheckingException {
-		Environment env = program.createEnvironment();
+		env = program.createEnvironment();
 		program.checkScope(env);
 		program.checkType(env);
 		program.checkConst(env);
-		// return program.check();
+		isValid = true;
 		return true;
 	}
 
+	public Program getProgram() {
+		return program;
+	}
+
+	public Environment getEnv() {
+		return env;
+	}
+
+	public boolean isValid() {
+		return isValid;
+	}
 }
