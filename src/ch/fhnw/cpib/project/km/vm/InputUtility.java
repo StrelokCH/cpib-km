8c335ec260c94beb7588ad2b03def038a1f71643
Janik Meyer <janik.meyer@students.fhnw.ch>
2019-01-03 12:06:09 +0100

    added VM to project

---
+++ src/ch/fhnw/cpib/project/km/vm/InputUtility.java
@@ -0,0 +1,45 @@
+// Virtual Machine Java 2015, V01
+// Edgar F.A. Lederer, FHNW and Uni Basel, 2015
+
+package ch.fhnw.cpib.project.km.vm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import ch.fhnw.cpib.project.km.vm.IVirtualMachine.ExecutionError;
+
+class InputUtility {
+
+	private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+	public static boolean readBool() throws ExecutionError {
+		String s;
+		try {
+			s = reader.readLine();
+		} catch (IOException e) {
+			throw new ExecutionError("Input failed.");
+		}
+		if (s.equals("false")) {
+			return false;
+		} else if (s.equals("true")) {
+			return true;
+		} else {
+			throw new ExecutionError("Not a boolean.");
+		}
+	}
+
+	public static int readInt() throws ExecutionError {
+		String s;
+		try {
+			s = reader.readLine();
+		} catch (IOException e) {
+			throw new ExecutionError("Input failed.");
+		}
+		try {
+			return Integer.parseInt(s);
+		} catch (NumberFormatException e) {
+			throw new ExecutionError("Not an integer.");
+		}
+	}
+}
